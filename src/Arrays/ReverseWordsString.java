ce11abea476bca851cc99b2aeb5a81b2ad23e498
SurendharGnanavel <surendhar02@gmail.com>
2023-05-17 21:20:48 +0530

    adding interviewPrograms

---
+++ src/Arrays/ReverseWordsString.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+
+public class ReverseWordsString {
+    public static void main(String[] args) {
+        String s = "the sky is blue";
+        reverseWords(s);
+    }
+
+    public static void reverseWords(String s){
+        String[] s1 = s.split("\\s+");
+        LinkedList<String> ll = new LinkedList<>(Arrays.asList(s1));
+        Iterator it = ll.descendingIterator();
+        while(it.hasNext()){
+            System.out.print(it.next()+" ");
+        }
+
+    }
+}
